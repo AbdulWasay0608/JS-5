487e68f2d81c98c399a1f088728320090aa11985
AbdulWasay0608 <abdulwasay0608@gmail.com>
2023-05-23 23:34:27 +0500

    <Work Complete>

---
+++ app.js
@@ -0,0 +1,43 @@
+var inp1 = document.getElementById('firstName')
+// var inp2 = document.getElementById('surName')
+// var inp3 = document.getElementById('phoneNo')
+// var inp4 = document.getElementById('newPassword')
+// var inp5 = document.getElementById('opt1')
+// var inp6 = document.getElementById('opt2')
+// var inp7 = document.getElementById('opt3')
+// function register() {
+//     alert (inp1.value)
+//     alert (inp2.value)
+//     alert (inp3.value)
+//     alert (inp4.value)
+//     alert (inp5.value)
+//     alert (inp6.value)
+//     alert (inp7.value)
+
+// }
+
+var inp1 = document.getElementById ('firstName')
+var inp2 = document.getElementById ('surName')
+var inp3 = document.getElementById ('phoneNo')
+var inp4 = document.getElementById ('newPassword')
+var inp5 = document.getElementById('opt1')
+var inp6 = document.getElementById('opt2')
+var inp7 = document.getElementById('birthDate')
+var inp8 = document.getElementById('birthMonth')
+var inp9 = document.getElementById('birthYear')
+var inp10 = document.getElementById('gender')
+
+
+function register() {
+    alert (inp1.value)
+    alert (inp2.value)
+    alert (inp3.value)
+    alert (inp4.value)
+    alert (inp6.value)
+    alert (inp7.value)
+    alert (inp8.value)
+    alert (inp9.value)
+    alert (inp10.value)
+
+}
+
